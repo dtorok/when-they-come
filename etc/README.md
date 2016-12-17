0e52e772a8c9d0ab3c193a7047f26dcdf13971a3
Daniel Torok <daniel.torok@prezi.com>
2016-12-17 14:56:39 +0100

    [web] simplest secret management ever

---
+++ etc/README.md
@@ -0,0 +1,8 @@
+# Configuration
+
+### Setting Google API Key
+
+To use google maps you'll have to [generate an API key](https://developers.google.com/maps/documentation/javascript/get-api-key).
+
+When you have that, put it into the `etc/google_api_key` file.
+
--- src/web/web.go
+++ src/web/web.go
@@ -3,6 +3,7 @@ package web
 import (
 	"net/http"
 	"text/template"
+	"io/ioutil"
 )
 
 func AddHandlers() {
@@ -22,11 +23,26 @@ func test(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, "")
 }
 
+func loadApiKey() (string, error) {
+	data, err := ioutil.ReadFile("etc/google_api_key.ini")
+	if err != nil {
+		return "", err
+	} else {
+		return string(data), nil
+	}
+}
+
 func main(w http.ResponseWriter, r *http.Request) {
+	google_api_key, err := loadApiKey()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	data := struct {
 		GoogleAPIKey string
 	}{
-		GoogleAPIKey: "SECRET",
+		GoogleAPIKey: google_api_key,
 	}
 	t, err := template.New("main.html").ParseFiles("src/web/templates/main.html")
 	if err != nil {
