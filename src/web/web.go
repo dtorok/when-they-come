d09f50f27b2b916812bb8120293cbce21aeec571
Daniel Torok <daniel.torok@prezi.com>
2016-12-17 14:25:22 +0100

    [web] fix static files + api_key to the template context

--- src/web/web.go
+++ src/web/web.go
@@ -6,18 +6,32 @@ import (
 )
 
 func AddHandlers() {
-	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("web/static")))
+	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("src/web/static")))
 
 	http.Handle("/static/", staticHandler)
 	http.HandleFunc("/test", test)
 	http.HandleFunc("/", main)
 }
 
-func main(w http.ResponseWriter, r *http.Request) {
-	t, err := template.New("main.html").Delims("{{{", "}}}").ParseFiles("src/web/templates/main.html")
+func test(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("test.html").Delims("{{{", "}}}").ParseFiles("src/web/templates/test.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	t.Execute(w, "")
+}
+
+func main(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		GoogleAPIKey string
+	}{
+		GoogleAPIKey: "SECRET",
+	}
+	t, err := template.New("main.html").ParseFiles("src/web/templates/main.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, data)
 }
